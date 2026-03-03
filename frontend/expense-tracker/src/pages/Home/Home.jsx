d810b5dff97e0e3dd41a348545a21f72a6e33fc2
ashuchauhan5120-hub <ashuchauhan5120@gmail.com>
2026-03-03 14:22:46 +0530

    add home page ui

---
+++ frontend/expense-tracker/src/pages/Home/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div id="home" className="home">
+      <div className="wrapper-home">
+
+        <div className="floating-icons">
+          <div className="flot-icon">📒</div>
+          <div className="flot-icon">🌸</div>
+          <div className="flot-icon">💛</div>
+          <div className="flot-icon">🌙</div>
+          <div className="flot-icon">🌱</div>
+          <div className="flot-icon">💬</div>
+        </div>
+
+
+        <div className="intro">
+          <h1 className="name-heading">camellia.</h1>
+          <p className="intro-para">Understand your relationship with money.</p>
+        </div>
+
+        <div className="about-sec">
+          <h2 className="heading-about">Reflect. Spend with intention.</h2>
+          <p className="about-para">
+            A calm, guided companion for understanding where your money actually
+            goes.
+          </p>
+        </div>
+
+        <div className="auth-sec">
+          <a href="/register"><button className="btn btn-one">I'm new here</button></a>
+          <a href="/login"><button className="btn btn-two">I already have an account</button></a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
